76f72d795ae0030199415c11664a7f477f640399
davidbarbi3r <davidbarbi3r@gmail.com>
2023-02-28 21:04:16 +0100

    :lipstick: style improvement

--- script/index.js
+++ script/index.js
@@ -1,16 +1,28 @@
 import { recipes } from "/script/recipes.js";
 
-const search = document.querySelector("#search");
-search.addEventListener("search", () => {
+const search = document.querySelector("#main_search");
+/* Listening for a keyup event on the search input. */
+search.addEventListener("keyup", () => {
   const searchValue = search.value;
+
+  if (searchValue.length < 3) {
+    return;
+  }
+
   const filteredRecipes = recipes.filter((recipe) => {
     return recipe.name.toLowerCase().includes(searchValue.toLowerCase());
   });
+  const filteredRecipesByIngredients = recipes.filter((recipe) => {
+    return recipe.ingredients.join(" ").includes(searchValue.toLowerCase())
+  });
+  const filteredRecipesByDescription = recipes.filter((recipe) => {
+    return recipe.description.includes(searchValue.toLowerCase())
+  })
   const recipesTest = structuredClone(recipes);
-
+  console.log(filteredRecipesByDescription)
+  console.log(recipes[0].ingredients.ingredient.join(" "))
+  console.log(filteredRecipesByIngredients);
   for (const element of recipesTest) {
     return element.name;
   }
-
-  console.log(recipesTest);
 });
