8cd9096405a2e92264a6e3ff725a1b524d967d95
davidbarbi3r <davidbarbi3r@gmail.com>
2023-03-02 19:27:12 +0100

    :construction: search function + render cards

--- script/index.js
+++ script/index.js
@@ -1,28 +1,62 @@
 import { recipes } from "/script/recipes.js";
+import { searchFunctional } from "/script/searchFunctional.js";
+
+const SEARCH_MODE = "functional"
 
 const search = document.querySelector("#main_search");
 /* Listening for a keyup event on the search input. */
 search.addEventListener("keyup", () => {
   const searchValue = search.value;
+  let filteredRecipes
 
   if (searchValue.length < 3) {
     return;
   }
 
-  const filteredRecipes = recipes.filter((recipe) => {
-    return recipe.name.toLowerCase().includes(searchValue.toLowerCase());
-  });
-  const filteredRecipesByIngredients = recipes.filter((recipe) => {
-    return recipe.ingredients.join(" ").includes(searchValue.toLowerCase())
-  });
-  const filteredRecipesByDescription = recipes.filter((recipe) => {
-    return recipe.description.includes(searchValue.toLowerCase())
-  })
-  const recipesTest = structuredClone(recipes);
-  console.log(filteredRecipesByDescription)
-  console.log(recipes[0].ingredients.ingredient.join(" "))
-  console.log(filteredRecipesByIngredients);
-  for (const element of recipesTest) {
-    return element.name;
+  if (SEARCH_MODE === "functional"){
+    filteredRecipes = searchFunctional(searchValue, recipes)
+  } else {
+    // native logic
+    const recipesTest = structuredClone(recipes);
+    for (const element of recipesTest) {
+      return element.name;
+    }
   }
+  console.log(filteredRecipes)
+  // render
+  const recipesContainer = document.querySelector(".recipes_container")
+  recipesContainer.innerHTML = `
+    ${filteredRecipes.map((recipe) => {
+      return `
+      <article>
+        <div class="img_container"></div>
+        <div>
+          <div>
+            <h2>${recipe.name}</h2>
+            <p>
+              <img src="/assets/time.svg" alt="clock logo"/>
+              ${recipe.time} min
+            </p>   
+          </div>
+          <div>
+            <div>${recipe.ingredients.map((i) => {
+              return `
+              <p>
+              ${i.ingredient}
+              ${i.quantity ? (": " + i.quantity + " "+ i.unit):""}
+              </p>   
+            `})}
+            </div>
+            <div>${recipe.description}</div>
+          </div>
+        </div>
+      </article>`
+    })}
+  `
 });
+const array = [{test: "test"}, {test:"test"}]
+// log an array of test propertie values
+console.log(array.map((element) => element.test))
+console.log()
+
+
---
+++ script/searchFunctional.js
@@ -0,0 +1,19 @@
+export const searchFunctional = (search, recipes) => {
+    const filteredRecipes = recipes.filter((recipe) => {
+        return recipe.name.toLowerCase().includes(search.toLowerCase());
+      });
+      const filteredRecipesByIngredients = recipes.filter((recipe) => {
+        return recipe.ingredients.map((ingredient) => ingredient.ingredient).join(" ").includes(search.toLowerCase())
+      });
+      const filteredRecipesByDescription = recipes.filter((recipe) => {
+        return recipe.description.includes(search.toLowerCase())
+      });
+      
+      return [...filteredRecipes, ...filteredRecipesByIngredients, ...filteredRecipesByDescription]
+        .reduce((acc,cur) => {
+        if (!acc.includes(cur)){
+            acc.push(cur)
+        }
+        return acc
+      },[])
+}
