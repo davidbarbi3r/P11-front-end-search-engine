d64b81f3f936d4331f1b43d22815f5cb1184bbf5
davidbarbi3r <davidbarbi3r@gmail.com>
2023-03-27 09:58:24 +0200

    :sparkles: renderTags

--- script/tagsFilters/renderTags.js
+++ script/tagsFilters/renderTags.js
@@ -1,7 +1,20 @@
 export function renderTags (tagsList, tags){
+    // render tags from tagsList
     tags.innerHTML = tagsList.map((tag) => {
         return `
-            <button>${tag.tag}</button>
+            <button class="tag_btn">
+                ${tag.tag}\u00A0\u00A0
+                <img class="close_btn" src="../../assets/cross.svg" alt="close"/>
+            </button>
         `
-    }).join("")
+    }).join("");
+
+    // add event listener to cross to remove tag from tagsList
+    const closeBtn = document.querySelectorAll(".close_btn");
+    closeBtn.forEach((el, key) => {
+        el.addEventListener("click", () => {
+            tagsList.splice(key, 1);
+            renderTags(tagsList, tags);
+        })
+    })
 }
