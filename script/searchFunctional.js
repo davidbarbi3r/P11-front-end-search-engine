839aec04c409a454a69d7d90e77f32c023867765
davidbarbi3r <davidbarbi3r@gmail.com>
2023-04-06 09:15:48 +0200

    :recycle: Refactor searchFunctional.js

--- script/searchFunctional.js
+++ script/searchFunctional.js
@@ -1,157 +1,35 @@
 export const searchFunctional = (search, recipes, tagsArray) => {
-  
-  const ingredientsTags = tagsArray.map((tag) => {
-    if (tag.type === "ingredient") {
-      return tag.tag;
-    } else {
-      return;
-    }
-  }).filter((tag) => tag !== undefined);
+  // search implementation with filter and map
+  const tags = tagsArray.map((tag) => tag.tag.toLowerCase());
 
-  const applianceTags = tagsArray.map((tag) => {
-    if (tag.type === "appliance") {
-      return tag.tag;
-    } else {
-      return;
-    }
-  }).filter((tag) => tag !== undefined);
-
-  const ustensilsTags = tagsArray.map((tag) => {
-    if (tag.type === "ustensil") {
-      return tag.tag;
-    } else {
-      return;
-    }
-  }).filter((tag) => tag !== undefined);
-
-  const filteredRecipes = recipes.filter((recipe) => {
-    return recipe.name.toLowerCase().includes(search.toLowerCase());
-  });
-  const filteredRecipesByIngredients = recipes.filter((recipe) => {
-    return recipe.ingredients
+  // filter recipes by search input
+  const searchedRecipes = recipes.filter((recipe) => {
+    const nameMatch = recipe.name.toLowerCase().includes(search.toLowerCase());
+    const ingredientMatch = recipe.ingredients
       .map((ingredient) => ingredient.ingredient.toLowerCase())
       .join(" ")
       .includes(search.toLowerCase());
+    const descriptionMatch = recipe.description
+      .toLowerCase()
+      .includes(search.toLowerCase());
+    return nameMatch || ingredientMatch || descriptionMatch;
   });
-  const filteredRecipesByDescription = recipes.filter((recipe) => {
-    return recipe.description.toLowerCase().includes(search.toLowerCase());
-  });
-
-  const searchedRecipes = [
-    ...filteredRecipes,
-    ...filteredRecipesByIngredients,
-    ...filteredRecipesByDescription,
-  ].reduce((acc, cur) => {
-    if (!acc.includes(cur)) {
-      acc.push(cur);
-    }
-    return acc;
-  }, []);
 
   if (!tagsArray.length) {
     return searchedRecipes;
   }
 
-  let filteredRecipesByTags = searchedRecipes;
-  // return recipes that contain all ingredients in tags
-  if(ingredientsTags.length && applianceTags.length && ustensilsTags.length){
-    filteredRecipesByTags = searchedRecipes.filter((recipe) => {
-      return ingredientsTags.every((ingredient) => {
-        return recipe.ingredients.some((recipeIngredient) => {
-          return (
-            recipeIngredient.ingredient.toLowerCase() === ingredient.toLowerCase()
-          );
-        });
-      });
-    }).filter((recipe) => {
-      return applianceTags.every((appliance) => {
-        return recipe.appliance.toLowerCase() === appliance.toLowerCase();
-      });
-    }).filter((recipe) => {
-      return ustensilsTags.every((ustensil) => {
-        return recipe.ustensils.some((recipeUstensil) => {
-          return recipeUstensil.toLowerCase() === ustensil.toLowerCase();
-        });
-      });
-    }
-    )
-    return filteredRecipesByTags;
-  } else if (ingredientsTags.length && applianceTags.length) {
-    filteredRecipesByTags = searchedRecipes.filter((recipe) => {
-      return ingredientsTags.every((ingredient) => {
-        return recipe.ingredients.some((recipeIngredient) => {
-          return (
-            recipeIngredient.ingredient.toLowerCase() === ingredient.toLowerCase()
-          );
-        });
-      });
-    }).filter((recipe) => {
-      return applianceTags.every((appliance) => {
-        return recipe.appliance.toLowerCase() === appliance.toLowerCase();
-      });
-    });
-    return filteredRecipesByTags;
-  } else if (ingredientsTags.length && ustensilsTags.length) {
-    filteredRecipesByTags = searchedRecipes.filter((recipe) => {
-      return ingredientsTags.every((ingredient) => {
-        return recipe.ingredients.some((recipeIngredient) => {
-          return (
-            recipeIngredient.ingredient.toLowerCase() === ingredient.toLowerCase()
-          );
-        });
-      });
-    }).filter((recipe) => {
-      return ustensilsTags.every((ustensil) => {
-        return recipe.ustensils.some((recipeUstensil) => {
-          return recipeUstensil.toLowerCase() === ustensil.toLowerCase();
-        });
-      });
-    });
-    return filteredRecipesByTags;
-  } else if (applianceTags.length && ustensilsTags.length) {
-    filteredRecipesByTags = searchedRecipes.filter((recipe) => {
-      return applianceTags.every((appliance) => {
-        return recipe.appliance.toLowerCase() === appliance.toLowerCase();
-      });
-    }).filter((recipe) => {
-      return ustensilsTags.every((ustensil) => {
-        return recipe.ustensils.some((recipeUstensil) => {
-          return recipeUstensil.toLowerCase() === ustensil.toLowerCase();
-        });
-      });
-    });
-    return filteredRecipesByTags;
-  }
-  if (ingredientsTags.length > 0) {
-    filteredRecipesByTags = searchedRecipes.filter((recipe) => {
-      return ingredientsTags.every((ingredient) => {
-        return recipe.ingredients.some((recipeIngredient) => {
-          return (
-            recipeIngredient.ingredient.toLowerCase() === ingredient.toLowerCase()
-          );
-        });
-      });
-    });
-    return filteredRecipesByTags;
-  }
-  if (applianceTags.length > 0) {
-    filteredRecipesByTags = searchedRecipes.filter((recipe) => {
-      return applianceTags.every((appliance) => {
-        return recipe.appliance.toLowerCase() === appliance.toLowerCase();
-      });
-    });
-    return filteredRecipesByTags;
-  }
-  if (ustensilsTags.length > 0) {
-    filteredRecipesByTags = searchedRecipes.filter((recipe) => {
-      return ustensilsTags.every((ustensil) => {
-        return recipe.ustensils.some((recipeUstensil) => {
-          return recipeUstensil.toLowerCase() === ustensil.toLowerCase();
-        });
-      });
-    });
-    return filteredRecipesByTags;
-  }
-  
+  // filter recipes by tags
+  const filteredRecipesByTags = searchedRecipes.filter((recipe) => {
+    const recipeTags = [
+      recipe.appliance.toLowerCase(),
+      ...recipe.ingredients.map((ingredient) =>
+        ingredient.ingredient.toLowerCase()
+      ),
+      ...recipe.ustensils.map((ustensil) => ustensil.toLowerCase()),
+    ];
+    return tags.every((tag) => recipeTags.includes(tag));
+  });
+
   return filteredRecipesByTags;
 };
